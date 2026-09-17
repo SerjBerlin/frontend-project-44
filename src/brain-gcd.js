687d3278b76b06162763e8fbe107bfdf9a7f97d8
SerjBerlin <bulanovsd@mail.ru>
2026-09-17 22:07:04 +0300

    Создание правил игры для brain-gcd

---
+++ src/brain-gcd.js
@@ -0,0 +1,19 @@
+import { generateNum, gameRule } from '../src/index.js'
+
+const description = `Find the greatest common divisor of given numbers.`
+
+const findGcd = () => {
+  let num1 = generateNum(1, 50)
+  let num2 = generateNum(1, 50)
+  const question = `${num1} ${num2}`
+  while (num2 !== 0) {
+    const temp = num2
+    num2 = num1 % num2
+    num1 = temp
+  }
+  return [question, String(num1)]
+}
+
+export default () => {
+  gameRule(description, findGcd)
+}
