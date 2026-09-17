957fd4250a2dfa9ac1a478536ea4d348e21afa10
SerjBerlin <bulanovsd@mail.ru>
2026-09-17 16:42:32 +0300

    Создание правил игры для brain-calc

---
+++ src/brain-calc.js
@@ -0,0 +1,29 @@
+import { generateNum, gameRule } from '../src/index.js'
+
+const symbolsOfMathematicalOperations = ['*', '+', '-']
+
+const description = `What is the result of the expression?`
+
+const gameData = () => {
+  const num1 = generateNum(1, 10)
+  const num2 = generateNum(1, 10)
+  let correctAnswer = ''
+  const operation = symbolsOfMathematicalOperations[Math.floor(Math.random() * (symbolsOfMathematicalOperations.length))]
+  const question = `${num1} ${operation} ${num2}`
+  switch (operation) {
+    case '+':
+      correctAnswer = num1 + num2
+      break
+    case '*':
+      correctAnswer = num1 * num2
+      break
+    case '-':
+      correctAnswer = num1 - num2
+      break
+  }
+  return [question, String(correctAnswer)]
+}
+
+export default () => {
+  gameRule(description, gameData)
+}
