bb8850320389c9c82cb3cddab8daf3a3ebc42b6c
SerjBerlin <bulanovsd@mail.ru>
2026-09-17 15:52:47 +0300

    Применение основной логики уже в существующей игре brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import evenOrNotGame from '../src/brain-even.js'
+import gameRule from '../src/brain-even.js'
 
-evenOrNotGame()
+gameRule()
--- src/brain-even.js
+++ src/brain-even.js
@@ -1,4 +1,4 @@
-import readlineSync from 'readline-sync'
+import { generateNum, gameRule } from '../src/index.js'
 
 const evenOrNot = (number) => {
   const num = number % 2 === 0 ? 'even' : 'odd'
@@ -12,33 +12,14 @@ const evenOrNot = (number) => {
   }
 }
 
-const generateNum = (min, max) => {
-  const num = Math.floor(Math.random() * (max - min + 1) + min)
-  return num
+const ruleGame = `Answer "yes" if the number is even, otherwise answer "no".`
+
+const gameEven = () => {
+  const question = generateNum(1, 50)
+  const correctAnswer = evenOrNot(question)
+  return [question, correctAnswer]
 }
 
-const evenOrNotGame = () => {
-  console.log('Welcome to the Brain Games!')
-  const name = readlineSync.question('May I have your name? ')
-  console.log(`Hello, ${name}!`)
-  console.log(`Answer "yes" if the number is even, otherwise answer "no".`)
-  let i = 0
-  while (i <= 2) {
-    const num = generateNum(1, 100)
-    const correctAnswer = evenOrNot(num)
-    console.log(`Question: ${num}`)
-    const answer = readlineSync.question('Your answer: ')
-    if (correctAnswer === answer) {
-      console.log('Correct!')
-    }
-    else {
-      console.log (`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
-      console.log(`Let's try again, ${name}!`)
-      return
-    }
-    i += 1
-  }
-  console.log(`Congratulations, ${name}!`)
+export default () => {
+  gameRule(ruleGame, gameEven)
 }
-
-export default evenOrNotGame
