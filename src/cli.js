d4f80811fce5df196d0fc73d6a2a90c14527f6ae
SerjBerlin <bulanovsd@mail.ru>
2026-09-17 15:53:44 +0300

    незначительное обновление cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,33 @@
+/*
 import readlineSync from 'readline-sync'
 
 const greeting = () => {
   console.log('Welcome to the Brain Games!')
   const name = readlineSync.question('May I have your name? ')
   console.log(`Hello, ${name}!`)
+  return name
 }
 
-export default greeting
+const correctAnswerOrNot = (answer, correctAnswer, name) => {
+  if (correctAnswer === answer) {
+    console.log('Correct!')
+    return true
+  }
+  else {
+    console.log (`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+    console.log(`Let's try again, ${name}!`)
+    return false
+  }
+}
+
+const congratulations = (name) => {
+  console.log(`Congratulations, ${name}!`)
+}
+
+const generateNum = (min, max) => {
+  const num = Math.floor(Math.random() * (max - min + 1) + min)
+  return num
+}
+
+export { correctAnswerOrNot, congratulations, generateNum }
+export default greeting */
