3904900021e69866fb529cdb81fe01a9e0f0905c
SerjBerlin <bulanovsd@mail.ru>
2026-09-20 00:01:45 +0300

    Возвращение в работу программы brain-games

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,3 @@
-/*
 import readlineSync from 'readline-sync'
 
 const greeting = () => {
@@ -8,26 +7,4 @@ const greeting = () => {
   return name
 }
 
-const correctAnswerOrNot = (answer, correctAnswer, name) => {
-  if (correctAnswer === answer) {
-    console.log('Correct!')
-    return true
-  }
-  else {
-    console.log (`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
-    console.log(`Let's try again, ${name}!`)
-    return false
-  }
-}
-
-const congratulations = (name) => {
-  console.log(`Congratulations, ${name}!`)
-}
-
-const generateNum = (min, max) => {
-  const num = Math.floor(Math.random() * (max - min + 1) + min)
-  return num
-}
-
-export { correctAnswerOrNot, congratulations, generateNum }
-export default greeting */
+export default greeting
